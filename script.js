e6ea82a62ca4a9f966c1ac6c9a002760dc9f0ee2
giresse31 <142321165+giresse31@users.noreply.github.com>
2026-02-27 12:43:21 +0100

    Modify payment button URL and retain functionality
    Updated payment button URL and ensured functionality remains intact.

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const PayBtn = document.querySelectorAll('.pay-btn');
 PayBtn.forEach(btn => {
   btn.addEventListener('click', (event) => {
     event.preventDefault();
-    window.open('https://bbzzdbpm.mychariow.shop/formation/checkout');
+    window.open('https://www.pay.moneyfusion.net/formation-masterclass_1753774062299/');
   });
 });
 
@@ -41,3 +41,4 @@ closeBtn.addEventListener('click', () => {
   overlay.style.display = 'none';
   document.body.classList.remove('noscroll');
 });
+
